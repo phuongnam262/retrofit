b3b459a2f0b185a5306102f95a6f944b407dea18
phuongnam <zdpnam1@.com>
2025-05-14 09:19:38 +0700

    deatail

--- app/src/main/java/com/example/retrofit/DetailActivity.kt
+++ app/src/main/java/com/example/retrofit/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.example.retrofit
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.example.retrofit.model.Product
 
 class DetailActivity : AppCompatActivity() {
@@ -18,7 +20,11 @@ class DetailActivity : AppCompatActivity() {
         id.text=product.id.toString()
         val title=findViewById<TextView>(R.id.title_detail)
         title.text=product.title
-
-
+        val price=findViewById<TextView>(R.id.price_detail)
+        price.text=product.price.toString()
+        val img=findViewById<ImageView>(R.id.product_img)
+        Glide.with(this)
+            .load(product.image)
+            .into(img)
     }
 }
