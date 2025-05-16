2d588c5b18a9c5205db6a538e05a18379117dadf
phuongnam <zdpnam1@.com>
2025-05-16 16:49:27 +0700

    new

--- app/src/main/java/com/example/retrofit/LoginActivity.kt
+++ app/src/main/java/com/example/retrofit/LoginActivity.kt
@@ -1,20 +1,32 @@
 package com.example.retrofit
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.retrofit.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var btnLogin: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        email=findViewById(R.id.et_email)
+        password=findViewById(R.id.et_password)
+        btnLogin=findViewById(R.id.btn_login)
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/retrofit/model/User.kt
@@ -0,0 +1,7 @@
+package com.example.retrofit.model
+
+data class User(
+    val id: String ? =null,
+    val username: String,
+    val password: String,
+)
